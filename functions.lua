9cba1e16f139c5f9e303097b9cefeecb90845a14
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-10-17 00:25:08 +0200

    Put the functions in another file

---
+++ functions.lua
@@ -0,0 +1,37 @@
+function start_game(nbr_player)
+    score1 = 0
+    score2 = 0
+
+    center_pads()
+    center_ball()
+
+    -- Ball goes towards player 2
+    if nbr_player == 1 then
+        ball.speed_x = default_speed
+        ball.speed_y = default_speed
+    end
+    -- Ball goes towards player 1
+    if nbr_player == 2 then
+        ball.speed_x = -default_speed
+        ball.speed_y = -default_speed
+    end
+end
+
+function center_pads()
+    pad1.y = love.graphics.getHeight() / 2 - pad1.height / 2
+    pad2.y = love.graphics.getHeight() / 2 - pad2.height / 2
+end
+
+function center_ball()
+    ball.x = love.graphics.getWidth() / 2 - ball.width / 2
+    ball.y = love.graphics.getHeight() / 2 - ball.height / 2
+end
+
+function invert_ball_speed_x()
+    ball.speed_x = -ball.speed_x
+end
+
+function invert_ball_speed_y()
+    ball.speed_y = -ball.speed_y
+end
+
--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
+require("functions")
+
 screen_padding = 10
-default_speed = 2
+default_speed = 3
 
 score1 = 0
 score2 = 0
@@ -25,43 +27,6 @@ ball.speed_x = default_speed
 ball.speed_y = default_speed
 
 arr_ball_trail = {}
-
-function start_game(nbr_player)
-    score1 = 0
-    score2 = 0
-
-    center_pads()
-    center_ball()
-
-    -- Ball goes towards player 2
-    if nbr_player == 1 then
-        ball.speed_x = default_speed
-        ball.speed_y = default_speed
-    end
-    -- Ball goes towards player 1
-    if nbr_player == 2 then
-        ball.speed_x = -default_speed
-        ball.speed_y = -default_speed
-    end
-end
-
-function center_pads()
-    pad1.y = love.graphics.getHeight() / 2 - pad1.height / 2
-    pad2.y = love.graphics.getHeight() / 2 - pad2.height / 2
-end
-
-function center_ball()
-    ball.x = love.graphics.getWidth() / 2 - ball.width / 2
-    ball.y = love.graphics.getHeight() / 2 - ball.height / 2
-end
-
-function invert_ball_speed_x()
-    ball.speed_x = -ball.speed_x
-end
-
-function invert_ball_speed_y()
-    ball.speed_y = -ball.speed_y
-end
 
 function love.load()
     -- Load resources in
